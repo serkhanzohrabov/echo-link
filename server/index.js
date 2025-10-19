985a712276d97fd4488872ea94a638da43075066
Serkhan <106021920+southsidescript@users.noreply.github.com>
2025-10-19 14:50:57 +0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const http    = require('http');
+const path    = require('path');
+const {Server} = require('socket.io');
+
+const port = process.env.PORT || 4000;
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(path.join(__dirname, '../client')));
+
+io.on('connection',(socket) => {
+  
+  console.log(' 🔵 Пользователь подключился', socket.id);
+  
+  let nickname = 'Anonymous';
+  
+    socket.on('chat message',(msg)=>{
+    const fullMessage = {user: nickname, message: msg};
+    console.log(`${nickname} : ${msg}`);
+    io.emit('chat message',fullMessage);
+   })
+
+   socket.on('set username', (username) => {
+     nickname = username || 'Anonymous';
+     console.log('Имя установлено', nickname);
+   });
+
+    socket.on('disconnect', () => {
+      console.log(
+        '🔴 Пользователь отключился',
+        socket.id
+      );
+    });
+});
+
+
+
+server.listen(port,"192.168.1.136",()=>{
+  console.log(`Сервер запущен на порту ${port}`)
+});
